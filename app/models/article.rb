6dde7e0a62452fe653aa791b1c40f8704570f1fe
Son DANG <son@tinypulse.com>
2017-05-10 15:38:37 +0700

    Son DANG - CoderSchool Prework for Ruby On Rails

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+  def self.search(search)
+    where("title  || body ILIKE ?", "%#{search}%")
+  end
+end
